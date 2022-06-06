6040927c89e1c7881dd11f9a2d6c2ee42f514c01
EC2 Default User <ec2-user@ip-172-31-0-202.ap-northeast-1.compute.internal>
2022-06-06 01:48:11 +0000

    [Add] 3-12-2

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
+  get '/books' => 'books/index'
+  get '/books/76310' => 'books/show'
+  get '/books/76310/edit' => 'books/edit'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/' => 'books/new'
+  post 'tweets' => 'books#create'
+  patch 'tweets/:id'  => 'books#update'
+  delete 'tweets/:id' => 'books#destroy'
 end
